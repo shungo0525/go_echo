0513afe9eb5e3b249eb7474fb9975e895021f20a
shungo0525 <shungo0525@gmail.com>
2020-11-21 16:23:40 +0900

    controllerに切り出し

--- controllers/controllers.go
+++ controllers/controllers.go
@@ -2,6 +2,7 @@ package Controllers
 
 import (
 	"net/http"
+	"strconv"
 
 	"github.com/labstack/echo"
 )
@@ -19,6 +20,48 @@ var users = []User{
 	{4, "user4", "email4"},
 }
 
+// MEMO: function名は大文字でないとだめ。
 func GetUsers(c echo.Context) error {
+	return c.JSON(http.StatusOK, users)
+}
+
+func ShowUser(c echo.Context) error {
+	user := new(User)
+
+	// string -> int
+	paramId, _ := strconv.Atoi(c.Param("id"))
+	for i := 0; i < len(users); i++ {
+		if paramId == users[i].Id {
+			user = &users[i]
+		}
+	}
+	return c.JSON(http.StatusOK, user)
+}
+
+func CreateUser(c echo.Context) error {
+	maxUserId := 0
+	for i := 0; i < len(users); i++ {
+		if maxUserId < users[i].Id {
+			maxUserId = users[i].Id
+		}
+	}
+	newUser := new(User)
+	newUser.Id = maxUserId + 1
+	newUser.Name = c.FormValue("name")
+	newUser.Email = c.FormValue("email")
+
+	users = append(users, *newUser)
+	return c.JSON(http.StatusOK, users)
+}
+
+func DeleteUser(c echo.Context) error {
+	var newUsers = []User{}
+	for i := 0; i < len(users); i++ {
+		paramsId, _ := strconv.Atoi(c.Param("id"))
+		if users[i].Id != paramsId {
+			newUsers = append(newUsers, users[i])
+		}
+	}
+	users = newUsers
 	return c.JSON(http.StatusOK, users)
 }
--- main.go
+++ main.go
@@ -3,7 +3,7 @@ package main
 import (
 	"./Controllers"
 	"net/http"
-	"strconv"
+	// "strconv"
 	// "fmt"
 	// "reflect"  型を確認できる ex)reflect.TypeOf(1)
 
@@ -25,66 +25,21 @@ var users = []User{
 func main() {
 	e := echo.New()
 	e.GET("/", displayHome)
+	
+	// controllersに切り出し
 	e.GET("/users", Controllers.GetUsers)
-	// e.GET("/users", getUsers)
-	e.GET("/params", getParams)
-	e.GET("/users/:id", showUser)
-	e.POST("/users", createUser)
-	e.DELETE("/users/:id", deleteUser)
+	e.GET("/users/:id", Controllers.ShowUser)
+	e.POST("/users", Controllers.CreateUser)
+	e.DELETE("/users/:id", Controllers.DeleteUser)
 	e.Logger.Fatal(e.Start(":8080"))
+
+	// e.GET("/users", getUsers)
+	// e.GET("/users/:id", showUser)
+	// e.POST("/users", createUser)
+	// e.GET("/params", getParams)
+	// e.DELETE("/users/:id", deleteUser)
 }
 
 func displayHome(c echo.Context) error {
 	return c.JSON(http.StatusOK, "HOME")
-}
-
-func getUsers(c echo.Context) error {
-	return c.JSON(http.StatusOK, users)
-}
-
-func showUser(c echo.Context) error {
-	user := new(User)
-
-	// string -> int
-	paramId, _ := strconv.Atoi(c.Param("id"))
-	for i := 0; i < len(users); i++ {
-		if paramId == users[i].Id {
-			user = &users[i]
-		}
-	}
-	return c.JSON(http.StatusOK, user)
-}
-
-func getParams(c echo.Context) error {
-	name := c.FormValue("name")
-	email := c.FormValue("email")
-	return c.JSON(http.StatusOK, name+email)
-}
-
-func createUser(c echo.Context) error {
-	maxUserId := 0
-	for i := 0; i < len(users); i++ {
-		if maxUserId < users[i].Id {
-			maxUserId = users[i].Id
-		}
-	}
-	newUser := new(User)
-	newUser.Id = maxUserId + 1
-	newUser.Name = c.FormValue("name")
-	newUser.Email = c.FormValue("email")
-
-	users = append(users, *newUser)
-	return c.JSON(http.StatusOK, users)
-}
-
-func deleteUser(c echo.Context) error {
-	var newUsers = []User{}
-	for i := 0; i < len(users); i++ {
-		paramsId, _ := strconv.Atoi(c.Param("id"))
-		if users[i].Id != paramsId {
-			newUsers = append(newUsers, users[i])
-		}
-	}
-	users = newUsers
-	return c.JSON(http.StatusOK, users)
 }
